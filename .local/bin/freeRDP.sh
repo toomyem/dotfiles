47cd866d82b5a7a4788f4546d7e9521704e430d6
toomyem <toomyem@gmail.com>
2023-06-28 23:44:18 +0200

    Changes in freeRDP

---
+++ .local/bin/freeRDP.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+read -s -p Pass: x
+[ -n "$x" ] || exit
+echo ""
+LOG=$HOME/.freerdp.log
+[ -e $LOG ] && rm -f $LOG
+
+nohup xfreerdp \
+    /v:atm001-z15g8 \
+    /d:DS /u:atm001 \
+    /audio-mode:1 \
+    /dynamic-resolution \
+    /floatbar:sticky:off,show:fullscreen \
+    /drive:H,$HOME \
+    /cert:ignore \
+    /p:$x > $LOG 2>&1 &
+
+sleep 5
+
+if [ $(cat $LOG | wc -l) -gt 0 ]
+then
+    cat $LOG
+    echo "Press Enter"
+    read x
+fi
+
