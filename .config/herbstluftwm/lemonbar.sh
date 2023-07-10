0f4175626f8dbaed0ecffc5addc37f0daeb3e41f
toomyem <toomyem@gmail.com>
2023-07-10 12:30:47 +0200

    Refactored lemonbar config

--- .config/herbstluftwm/lemonbar.sh
+++ .config/herbstluftwm/lemonbar.sh
@@ -14,146 +14,156 @@ widgets_right=(player memory disk cpuload xbps datetime)
 CACHEDIR=$HOME/.cache/lemonbar
 SOCK=$CACHEDIR/lemon.sock
 
-[ -d $CACHEDIR ] || mkdir -p $CACHEDIR
-[ -e $SOCK ] && rm $SOCK
-mkfifo $SOCK
+[ -d "$CACHEDIR" ] || mkdir -p "$CACHEDIR"
+[ -e "$SOCK" ] && rm "$SOCK"
+mkfifo "$SOCK"
 
 declare -A widgets
 
 cleanup() {
-    rm $SOCK
+	rm "$SOCK"
 }
 
 sep() {
-    echo " | "
+	echo " | "
 }
 
 datetime() {
-    while true
-    do
-        echo "set:datetime:%{T2}%{T-} $(date '+%a %d/%m/%Y %H:%M:%S')" > $SOCK
-        sleep 1
-    done
+	while true; do
+		echo "set:datetime:%{T2}%{T-} $(date '+%a %d/%m/%Y %H:%M:%S')" >"$SOCK"
+		sleep 1
+	done
 }
 
 datetime &
 
 xbps() {
-    local prev_updates=0
-    while true
-    do
-        local updates=$(xbps-install -un | wc -l)
-        echo "set:xbps:%{A:xbps:}%{T2}%{T-} $updates%{A}" > $SOCK
-        if [ $updates -gt $prev_updates ]
-        then
-            prev_updates=$updates
-            notify-send -t 10000 Updates! "$(xbps-install -un | cut -d' ' -f1)"
-        fi
-        sleep 60
-    done
+	local prev_updates=0
+	local updates
+	while true; do
+		updates=$(xbps-install -un | wc -l)
+		echo "set:xbps:%{A:xbps:}%{T2}%{T-} $updates%{A}" >"$SOCK"
+		if [ "$updates" -eq 0 ]; then
+			prev_updates=0
+		elif [ "$updates" -gt "$prev_updates" ]; then
+			prev_updates="$updates"
+			notify-send -t 10000 Updates! "$(xbps-install -un | cut -d' ' -f1)"
+		fi
+		sleep 60
+	done
 }
 
 xbps &
 
 cpuload() {
-    while true
-    do
-        echo "set:cpuload:%{T2}%{T-} $(LANG=C uptime | sed -e 's/.*: //' -e 's/,//g')" > $SOCK
-        sleep 1
-    done
+	while true; do
+		echo "set:cpuload:%{T2}%{T-} $(LANG=C uptime | sed -e 's/.*: //' -e 's/,//g')" >"$SOCK"
+		sleep 1
+	done
 }
 
 cpuload &
 
 memory() {
-    while true
-    do
-        local m=$(LANG=C free -h | awk '/Mem/ {print $3}')
-        echo "set:mamory:%{T2}%{T-}$m" > $SOCK
-        sleep 1
-    done
+	local m
+	while true; do
+		m=$(LANG=C free -h | awk '/Mem/ {print $3}')
+		echo "set:mamory:%{T2}%{T-}$m" >"$SOCK"
+		sleep 1
+	done
 }
 
 memory &
 
 disk() {
-    while true
-    do
-        local d=$(df -h --output=target,used,size / | tail +2 | awk '{out=" "out$1" "$2"/"$3} END {print out}')
-        echo "set:disk:%{T2}%{T-}$d" > $SOCK
-        sleep 60
-    done
+	local d
+	while true; do
+		d=$(df -h --output=target,used,size / | tail +2 | awk '{out=" "out$1" "$2"/"$3} END {print out}')
+		echo "set:disk:%{T2}%{T-}$d" >"$SOCK"
+		sleep 60
+	done
 }
 
 disk &
 
 hlwm() {
-    while true
-    do
-        local n=$(herbstclient attr tags.count)
-        local tags=""
+	local n
+	local tags
+	local name
+	local title
 
-        for i in $(seq 0 $((n-1)))
-        do
-            local name=$(herbstclient attr tags.$i.name)
-            color="%{F$tag_normal}"
-            [ $(herbstclient attr tags.$i.client_count) -eq 0 ] && color="%{F$tag_empty}"
-            [ $(herbstclient attr tags.$i.urgent_count) -gt 0 ] && color="%{F$tag_urgent}"
-            [ $(herbstclient attr tags.focus.index) -eq $i ] && name="[$name]"
-            tags="$tags $color%{A:switch-to-$i:}$name%{A}%{F-}"
-        done
+	while true; do
+		n=$(herbstclient attr tags.count)
+		tags=""
 
-        local title="$(herbstclient attr clients.focus.title 2> /dev/null)"
-        [ ${#title} -gt 30 ] && title="${title:0:30}…"
+		for i in $(seq 0 $((n - 1))); do
+			name=$(herbstclient attr tags."$i".name)
+			color="%{F$tag_normal}"
+			[ "$(herbstclient attr tags.$i.client_count)" -eq 0 ] && color="%{F$tag_empty}"
+			[ "$(herbstclient attr tags.$i.urgent_count)" -gt 0 ] && color="%{F$tag_urgent}"
+			[ "$(herbstclient attr tags.focus.index)" -eq "$i" ] && name="[$name]"
+			tags="$tags $color%{A:switch-to-$i:}$name%{A}%{F-}"
+		done
 
-        echo "set:hlwm:$tags | $title" > $SOCK
-        sleep 1
-    done
+		title="$(herbstclient attr clients.focus.title 2>/dev/null)"
+		[ ${#title} -gt 30 ] && title="${title:0:30}…"
+
+		echo "set:hlwm:$tags | $title" >"$SOCK"
+		sleep 1
+	done
 }
 
 hlwm &
 
 player() {
-    local cmd
-    playerctl -F -p playerctld -f "{{status}}:{{trunc(title, 30)}} ({{trunc(artist, 20)}})" metadata | while read -r cmd
-    do
-        local status="${cmd/:*/}"
-        cmd=${cmd#$status:}
-        case "$status" in
-            Playing) cmd="$cmd %{T2}%{T-}" ;;
-            Paused) cmd="$cmd %{T2}%{T-}" ;;
-        esac
-        echo "set:player:%{A:play-pause:}$cmd%{A}" > $SOCK
-    done
+	local cmd
+	while true
+	do
+		playerctl -F -p playerctld -f "{{status}}:{{trunc(title, 30)}} ({{trunc(artist, 20)}})" metadata | while true; do
+			read -r -t 10 cmd || true
+			[ -z "$cmd" ] && cmd="$(playerctl -s -p playerctld -f "{{status}}:{{trunc(title, 30)}} ({{trunc(artist, 20)}})" metadata)"
+			case "$cmd" in
+			Playing:*) cmd="${cmd#Playing:} %{T2}%{T-}" ;;
+			Paused:*) cmd="${cmd#Paused:} %{T2}%{T-}" ;;
+			Stopped:*) cmd="${cmd#Stopped:} %{T2}%{T-}" ;;
+			*) cmd="" ;;
+			esac
+			[ -n "$cmd" ] && cmd="%{A:play-pause:}$cmd%{A}"
+			echo "set:player:$cmd" >"$SOCK"
+		done
+	done
 }
 
 player &
 
 show() {
-    local s=""
+	local s=""
 
-    for i;
-    do
-        local w="${widgets[$i]}"
-        [ "$s" != "" -a "$w" != "" ] && s="$s$(sep)"
-        s="$s$w"
-    done
-    echo $s
+	for i; do
+		local w="${widgets[$i]}"
+		[ "$s" != "" ] && [ "$w" != "" ] && s="$s$(sep)"
+		s="$s$w"
+	done
+	echo "$s"
 }
 
 trap cleanup EXIT
 
 herbstclient watch tags.focus.index
 
-while true
-do
-    read -r -t 1 cmd || true
-    case "$cmd" in
-        play-pause) playerctl -p playerctld play-pause ;;
-        switch-to-*) herbstclient use_index ${cmd#switch-to-} ;;
-        xbps) alacritty --class Scratchpad -e sudo /bin/xbps-install -u ;;
-        set:*) cmd=${cmd#set:} ; v=${cmd/:*/} ; cmd=${cmd#$v:} ; widgets[$v]="$cmd" ;;
-    esac
-    echo "%{l}$(show ${widgets_left[@]})%{c}$(show ${widgets_center[@]})%{r}$(show ${widgets_right[@]})"
-done < $SOCK | lemonbar -g 1920x28+1920+0 -p -a 20 -B $bar_bg_color -f "CaskaydiaCove NFM:style=Regular:size=12" -f "Font Awesome 6 Free:style=Solid:size=14" > $SOCK
+while true; do
+	read -r -t 1 cmd || true
+	case "$cmd" in
+	play-pause) playerctl -p playerctld play-pause ;;
+	switch-to-*) herbstclient use_index "${cmd#switch-to-}" ;;
+	xbps) alacritty --class Scratchpad -e sudo /bin/xbps-install -u ;;
+	set:*)
+		cmd=${cmd#set:}
+		v=${cmd/:*/}
+		cmd=${cmd#"$v":}
+		widgets[$v]="$cmd"
+		;;
+	esac
+	echo "%{l}$(show ${widgets_left[@]})%{c}$(show ${widgets_center[@]})%{r}$(show ${widgets_right[@]})"
+done <"$SOCK" | lemonbar -g 1920x28+1920+0 -p -a 20 -B "$bar_bg_color" -f "CaskaydiaCove NFM:style=Regular:size=12" -f "Font Awesome 6 Free:style=Solid:size=14" >"$SOCK"
 
